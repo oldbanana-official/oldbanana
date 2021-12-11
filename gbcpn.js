9b2e6b7f58d617c2e556445613ba542045e05ca6
Kur <42302831+fluffapparition@users.noreply.github.com>
2021-12-10 18:57:44 -0600

    Licensing

--- gbcpn.js
+++ gbcpn.js
@@ -1,15 +1,20 @@
-// BONKMAYKR'S CUSTOM PRIMARY NAV
-// THIS IS WHAT HAPPENS WHEN YOU DON'T LISTEN TO YOUR USERS
-// ....................../´¯/) 
-// ....................,/¯../ 
-// .................../..../ 
-// ............./´¯/'...'/´¯¯`·¸ 
-// ........../'/.../..../......./¨¯\ 
-// ........('(...´...´.... ¯~/'...') 
-// .........\.................'...../ 
-// ..........''...\.......... _.·´ 
-// ............\..............( 
-// ..............\.............\...
+/*
+*   Oldbanana - The Gamebanana Enhancement Project, for Firefox
+*   Copyright (C) 2021  bonkmaykr
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 /*var gbcpnVersionString = "1.0";
 window.console.info("[GBCPN] Gamebanana Custom Primary Nav, by bonkmaykr");
