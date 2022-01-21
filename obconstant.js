3f7a6be5579163117f3e7dafaa3255c3412d1fc5
Kur <42302831+fluffapparition@users.noreply.github.com>
2022-01-21 06:30:29 -0600

    inject stylesheet from the web
    - show icons on dev avatars

---
+++ obconstant.js
@@ -0,0 +1,3 @@
+var obDeveloperIconsSheet = document.createElement("style");
+obDeveloperIconsSheet.innerHTML = "@import url(https://oldbanana-official.github.io/auto-web-content/developers.css);";
+document.head.appendChild(obDeveloperIconsSheet);
