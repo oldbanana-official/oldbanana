274731b2e76e3ac369d444d901a51fbcb2f3b4ae
Kur <42302831+fluffapparition@users.noreply.github.com>
2022-01-21 06:59:54 -0600

    Whoops

--- obconstant.js
+++ obconstant.js
@@ -1,3 +1,21 @@
+/*
+*   Oldbanana - The Gamebanana Enhancement Project, for Firefox
+*   Copyright (C) 2021  bonkmaykr
+*
+*   This program is free software: you can redistribute it and/or modify
+*   it under the terms of the GNU General Public License as published by
+*   the Free Software Foundation, either version 3 of the License, or
+*   (at your option) any later version.
+*
+*   This program is distributed in the hope that it will be useful,
+*   but WITHOUT ANY WARRANTY; without even the implied warranty of
+*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*   GNU General Public License for more details.
+*
+*   You should have received a copy of the GNU General Public License
+*   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 var obDeveloperIconsSheet = document.createElement("style");
 obDeveloperIconsSheet.innerHTML = "@import url(https://oldbanana-official.github.io/auto-web-content/developers.css);";
 document.head.appendChild(obDeveloperIconsSheet);
